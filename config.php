4cfe7f817b8fbcd11a23f299c75cb2d9fe665c81
BeenReported <76714308+BeenReported@users.noreply.github.com>
2021-03-12 05:41:25 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,13 @@
+<!-- 
+
+Instructions:
+
+Firstly you need to register an account over on xapi.us in order to get your credentials.
+
+Secondly one you've signed in you can go to this link for your UXID and API Key https://xapi.us/profile.
+
+-->
+
 <?php
 
 $uxid = rawurlencode("UXID");
